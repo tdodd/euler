3ba596ac42cf49560af98940de4dbc3954e14b07
Thomas Dodd <thomas.dodd0@gmail.com>
2019-01-12 17:17:04 -0500

    Optimize Fibonacci (2)

--- src/2/test.ts
+++ src/2/test.ts
@@ -3,19 +3,25 @@ import { Fibonnaci } from "./main";
 import { expect } from "chai";
 
 describe("2: Even Fibbonaci Sum", () => {
+    //             0  1  2  3  4  5  6  7   8   9   10  11  12   13   14
+    const terms = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
     
     it("Regular fibonnaci", () => {
-        const terms = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
-
-        for (let i = 0; i < terms.length; i++) {
+        for (let i = 1; i < terms.length; i++) {
             expect(Fibonnaci.fibbonaci(i)).to.equal(terms[i]);
         }
     });
     
     it("Even fibbonaci up to 4 million", () => {
         let max = 4000000;
-        let sum = 3524577;
-        expect(Fibonnaci.fibbonaciSum(max)).to.equal(sum);
+
+        expect(Fibonnaci.evenFibbonaci(0, max)).to.equal(0);
+        expect(Fibonnaci.evenFibbonaci(1, max)).to.equal(0);
+        expect(Fibonnaci.evenFibbonaci(3, max)).to.equal(2);
+        expect(Fibonnaci.evenFibbonaci(4, max)).to.equal(2);
+        expect(Fibonnaci.evenFibbonaci(6, max)).to.equal(10);
+        expect(Fibonnaci.evenFibbonaci(10, max)).to.equal(44);
+        expect(Fibonnaci.evenFibbonaci(3524577, max)).to.equal(4613732);
     });
 
 });
