8befe663b0943119eb6c597da6faaf10be2549e3
Thomas Dodd <thomas.dodd0@gmail.com>
2019-03-24 12:25:43 -0400

    Create scripts directory
    * Move travis and code coverage commands into /scripts folder

---
+++ scripts/coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Code coverage script
+#
+
+# Configuration
+TEST_FILES="src/**/test.js"
+TEST_TIMEOUT=5000
+REPORT_DIR="code_coverage/report"
+TEMP_DIR="code_coverage/json"
+
+# Istanbul command + options
+node node_modules/nyc/bin/nyc \
+    --report-dir ${REPORT_DIR} \
+    --temp-directory ${TEMP_DIR} \
+    node_modules/mocha/bin/_mocha --timeout ${TEST_TIMEOUT} ${TEST_FILES}
