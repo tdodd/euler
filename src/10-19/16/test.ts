231cd8a3cb441e3d19cd3f2f89f779ef26d919d1
Thomas Dodd <thomas.dodd0@gmail.com>
2019-04-14 20:31:02 -0400

    Solve #16 - Power digit sum

---
+++ src/10-19/16/test.ts
@@ -0,0 +1,15 @@
+import { PowerDigitSum } from "./main";
+
+import { expect } from "chai";
+
+describe.only("16: Power Digit Sum", () => {
+    
+    it("Power digit sum of a small power (2^15)", () => {
+        expect(PowerDigitSum.getSum(2, 15)).to.equal(26);
+    });
+    
+    it("Power digit sum of a large power (2^1000)", () => {
+        expect(PowerDigitSum.getSum(2, 1000)).to.equal(1366);
+    });
+
+});
