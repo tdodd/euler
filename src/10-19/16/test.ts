691498c642c8bac5444f2e49ce3202eb6101c1d9
Thomas Dodd <thomas.dodd0@gmail.com>
2019-04-14 20:33:19 -0400

    Remove .only

--- src/10-19/16/test.ts
+++ src/10-19/16/test.ts
@@ -2,7 +2,7 @@ import { PowerDigitSum } from "./main";
 
 import { expect } from "chai";
 
-describe.only("16: Power Digit Sum", () => {
+describe("16: Power Digit Sum", () => {
     
     it("Power digit sum of a small power (2^15)", () => {
         expect(PowerDigitSum.getSum(2, 15)).to.equal(26);
