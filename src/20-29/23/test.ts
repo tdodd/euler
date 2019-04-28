36a1267b0f3ee5356533cc0698cce3502109815d
Thomas Dodd <thomas.dodd0@gmail.com>
2019-04-28 14:02:29 -0400

    Add boilerplate for 23

---
+++ src/20-29/23/test.ts
@@ -0,0 +1,12 @@
+import { AbundantNumbers } from "./main";
+
+import { expect } from "chai";
+
+
+describe.only("23: Abundant Numbers", () => {
+    
+    it("Sum of all positive integers", () => {
+        expect(AbundantNumbers.getSum()).to.equal(0);
+    });
+
+});
