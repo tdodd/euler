75e1862ab5ddb9c4dee3ab8688a9987cae94fa6f
Thomas Dodd <thomas.dodd0@gmail.com>
2019-05-11 09:30:34 -0400

    Comma

--- src/20-29/23/main.ts
+++ src/20-29/23/main.ts
@@ -11,7 +11,7 @@
  * As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
  * the smallest number that can be written as the sum of two abundant numbers is 24.
  * 
- * By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers.
+ * By mathematical analysis, it can be shown that all integers greater than 28,123 can be written as the sum of two abundant numbers.
  * However, this upper limit cannot be reduced any further by analysis even though it is known that
  * the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
  * 
