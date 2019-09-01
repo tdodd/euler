c96df3c55f6c5aa59c21ee154129f1ee3165e949
Thomas Dodd <thomas.dodd0@gmail.com>
2019-09-01 13:49:46 -0400

    WIP: get diagonal sum

--- src/20-29/28/main.ts
+++ src/20-29/28/main.ts
@@ -3,12 +3,12 @@
  * 
  * Starting with the number 1 and moving to the right in a clockwise direction, a 5 by 5 spiral is formed as follows:
  * 
- *      21 22 23 24 25
- *      20  7  8  9 10
- *      19  6  1  2 11
- *      18  5  4  3 12
- *      17 16 15 14 13
- * 
+ *          21  22 23 24 25
+ *          20  7  8  9  10
+ *          19  6  1  2  11
+ *          18  5  4  3  12
+ *          17 16 15 14  13
+ *  
  * It can be verified that the sum of the numbers on the diagonals is 101.
  * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
  */
@@ -16,7 +16,85 @@
 export class NumberSpiral {
 
     public static getSum(n: number): number {
-        return n;
+        let sum = 0;
+
+        // First, build the nxn matrix
+        let matrix = NumberSpiral.buildMatrix(n);
+
+        // Once the matrix is built, calculate the sum of the diagonals
+        for (let row = 0; row < n; row++) {
+            for (let col = 0; col < n; col++) {
+                let d1 = matrix[row][col];
+                let d2 = matrix[(matrix.length - 1) - row][(matrix.length - 1) - col];
+                sum += d1 + d2;
+            }
+        }
+
+        return sum;
+    }
+
+    // Build a clockwise spiral nxn matrix
+    private static buildMatrix(n: number): number[][] {
+        let matrix: number[][] = NumberSpiral.getEmptyMatrix(n);
+        let rowIdx = NumberSpiral.getCenter(n);
+        let colIdx = NumberSpiral.getCenter(n);
+        let dir = 0; // 0=RIGHT, 1=DOWN, 2=LEFT, 3=UP
+        let chain = 1; // Current chain size
+        let ctr = 1;
+                    
+        for (let i = 1; i < n; i++) {
+            for (let j = 0; j < (i < (n - 1) ? 2 : 3); j++) { // Change direction, and continue for the length of the chain
+                for (let k = 0; k < chain; k++) { // Print the current cell, and shift by 1 in current direction
+                    matrix[rowIdx][colIdx] = ctr++;
+                    switch (dir) {
+                        case 3: // UP
+                            rowIdx--;
+                            break;
+                        case 2: // LEFT
+                            colIdx--;
+                            break;
+                        case 1: // DOWN
+                            rowIdx++;
+                            break;
+                        case 0: // RIGHT
+                            colIdx++;
+                            break;
+                        default:
+                            throw new Error("Invalid direction");
+                    }
+                }
+                dir = (dir + 1) % 4; // Change direction when the chain is done
+            }
+            chain++;
+        }
+
+        matrix[rowIdx][colIdx] = ctr;
+        
+        return matrix;
+    }
+    
+    // Init an empty nxn matrix
+    private static getEmptyMatrix(n: number): number[][] {
+        let matrix: number[][] = [];
+
+        for (let i = 0; i < n; i++) {
+            matrix[i] = [];
+            for (let j = 0; j < n; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        return matrix;
+    }
+
+    // Get the center coordinates of an nxn matrix
+    public static getCenter(n: number): number {
+        let isEven = n % 2 === 0;
+        let center = Math.floor(n/2);
+        if (!isEven) {
+            return center
+        }
+        return center - 1;
     }
 
 }
--- src/20-29/28/test.ts
+++ src/20-29/28/test.ts
@@ -5,12 +5,28 @@ import { expect } from "chai";
 
 describe.only("28: Sum of the diagonals in an nxn spiral", () => {
     
+    it("2x2 spiral", () => {
+        expect(NumberSpiral.getSum(2)).to.equal(10);
+    });
+
+    it("3x3 spiral", () => {
+        expect(NumberSpiral.getSum(3)).to.equal(25);
+    });
+
+    it("4x4 spiral", () => {
+        expect(NumberSpiral.getSum(4)).to.equal(56);
+    });
+
     it("5x5 spiral", () => {
         expect(NumberSpiral.getSum(5)).to.equal(101);
     });
 
-    it("1001x1001 spiral", () => {
-        expect(NumberSpiral.getSum(1001)).to.equal(669171001);
+    it("6x6 spiral", () => {
+        expect(NumberSpiral.getSum(6)).to.equal(-1);
     });
+
+    // it("1001x1001 spiral", () => {
+    //     expect(NumberSpiral.getSum(1001)).to.equal(669171001);
+    // });
 
 });
