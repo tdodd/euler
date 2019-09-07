a3fe2bfb726437e32de6db286dd2ac9fcbaceefb
Thomas Dodd <thomas.dodd0@gmail.com>
2019-09-07 13:17:04 -0400

    Solve #27 - Consecutive Primes

--- src/20-29/27/main.ts
+++ src/20-29/27/main.ts
@@ -1,30 +1,73 @@
 /**
  * https://projecteuler.net/problem=27
  * 
- * Euler discovered the remarkable quadratic formula: n^2 + n + 41
+ * Euler discovered the quadratic formula: n^2 + n + 41
+ * The formula will produce 40 primes for the consecutive integer values 0 <= n <= 39
  * 
- * It turns out that the formula will produce 40 primes for the consecutive integer values 0 <= n <= 39
- * 
- * However, when n = 40, 40^2 + 40 + 41 = 40(40+1) + 41 is divisible by 41
- * and certainly when n = 41, 41^2 + 41 + 41 is clearly divisible by 41.
- * 
- * The incredible formula n^2 − 79n + 1601 was discovered
- * which produces 80 primes for the consecutive values 0 <= n <= 79
- * 
+ * The formula n^2 − 79n + 1601 produces 80 primes for the consecutive values 0 <= n <= 79
  * The product of the coefficients, −79 and 1601, is −126,479
  * 
  * Considering quadratics of the form: n^2 + an + b, where |a| < 1000 and |b| <= 1000
+ * where |n| is the absolute value of n (e.g. |11| = 11 and |−4| = 4)
  * 
- * where |n| is the modulus/absolute value of n
- * e.g. |11| = 11 and |−4| = 4
- * 
- * Find the product of the coefficients a and b
- * for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
+ * Find the product of the coefficients a and b for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
  */
 
 export class QuadraticPrimes {
+    private static cache: { [num: string]: boolean } = {};
 
-    public static getProduct(n: number): number {
+    public static getProduct(limit: number): number {
+        let currentMax = 0;
+        let product = 0;
+
+        for (let a = (-1 * limit); a < limit; a++) {
+            for (let b = (-1 * limit); b <= limit; b++) {
+                let consecutivePrimes = this.countConsecutivePrimes(a, b);
+                if (consecutivePrimes > currentMax) {
+                    currentMax = consecutivePrimes;
+                    product = a * b;
+                }
+            }
+        }
+
+        return product;
+    }
+
+    // Test whether a number is prime or not
+    // Primes are only divisible by 1 and themselves
+    private static isPrime(n: number): boolean {
+        // Check the cache first
+        if (this.cache.hasOwnProperty(n)) {
+            return this.cache[n];
+        }
+
+        // If it's not in the cache, compute
+        for (let i = 2; i < Math.abs(n); i++) {
+            if (n % i === 0) {
+                Object.defineProperty(this.cache, n.toString(), { value: false, enumerable: true });
+                return false;
+            }
+        }
+
+        Object.defineProperty(this.cache, n.toString(), { value: true, enumerable: true });
+        return true;
+    }
+
+    // Count the number of consecutive primes for a given formula
+    private static countConsecutivePrimes(a: number, b: number): number {
+        let n = 0;
+        let done = false;
+
+        while (!done) {
+            let formula = Math.pow(n, 2) + (a * n) + b;
+            let isPrime = this.isPrime(formula);
+            if (!isPrime) {
+                done = true;
+            } else {
+                n++;
+            }
+        }
+
         return n;
     }
 
--- src/20-29/27/test.ts
+++ src/20-29/27/test.ts
@@ -3,10 +3,10 @@ import { QuadraticPrimes } from "./main";
 import { expect } from "chai";
 
 
-describe.only("27: Quadratic Primes", () => {
+describe("27: Quadratic Primes", () => {
     
-    it("Maximum number of primes for consecutive values of n", () => {
-        expect(QuadraticPrimes.getProduct(-1)).to.equal(-59231);
+    it("Maximum number of primes for consecutive values of n < 1000", () => {
+        expect(QuadraticPrimes.getProduct(1000)).to.equal(-59231);
     });
 
 });
